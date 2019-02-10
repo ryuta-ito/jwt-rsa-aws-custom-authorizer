edcd5678a7992e877a6a1157ffa671571cdb7bf9
ryuta-ito <ir47839@gmail.com>
2019-02-10 12:33:13 +0900

    fix build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -4,7 +4,7 @@ set -e
 
 cd `dirname $0`/../
 
-zip -r jwt-rsa-aws-custom-authorizer.zip * -x event.json*
+zip -r jwt-rsa-aws-custom-authorizer.zip * -x event.json* -x jwt-rsa-aws-custom-authorizer.zip
 
 cd -
 
